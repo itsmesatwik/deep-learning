77df7266124446b47117617d661e85f4d4876511
satwik <satwik.illini@gmail.com>
2019-01-28 01:48:24 -0600

    testing

--- hw1/sgd.py
+++ hw1/sgd.py
@@ -13,7 +13,7 @@ def f_softmax(vec):
         vec[i] = np.exp(vec[i])
     sum_ = sum(vec)
     for i in vec:
-        ret[i] = vec[i] / sum_
+        ret[i] = vec[i] / np.float(sum_)
     return ret
 
 
@@ -59,28 +59,28 @@ def loss_grad(theta, x, y):
 
 
 def theta_update(theta, grad, ALPHA):
-    updated_theta = [[0] * 10 for _ in range(784)]
-    for i in range(784):
-        for j in range(10):
-            updated_theta[i][j] = theta[i][j] - ALPHA * grad[i][j]
+    #updated_theta = [[0] * 784 for _ in range(10)]
+    for i in range(10):
+        for j in range(784):
+            theta[i][j] -= (ALPHA * grad[i][j])
 
-    return updated_theta
+    return theta
 
 
 def mini_batch(x, y, theta, M_):
     grad_sum = [[0]*784 for _ in range(10)]
-    for i in range(M_):
-        new_grad = loss_grad(theta, x[i], y[i])
+    for i in range(100):
+        new_grad = loss_grad(theta, x[i + M_][:], y[i+M_])
         grad_sum = matrix_sum(grad_sum, new_grad)
 
     for i in range(10):
         for j in range(784):
-            grad_sum[i][j] = (grad_sum[i][j] / M_)
+            grad_sum[i][j] = (grad_sum[i][j] / np.float(M_))
     return grad_sum
 
 
 # load MNIST data
-MNIST_data = h5py.File('MNISTdata.hdf5', 'r')
+MNIST_data = h5py.File('../MNISTdata.hdf5', 'r')
 
 x_train = np.float32(MNIST_data['x_train'][:])
 y_train = np.int32(np.array(MNIST_data['y_train'][:, 0]))
@@ -100,27 +100,42 @@ num_inputs = 28 * 28
 
 num_outputs = 10
 
-model = {'W1': np.random.randn(num_outputs, num_inputs) / np.sqrt(num_inputs)}
+#model = {'W1': np.random.randn(num_outputs, num_inputs) / np.sqrt(num_inputs)}
 
-model_grads = copy.deepcopy(model)
+#model_grads = copy.deepcopy(model)
 
-r = np.random.random_sample()  # COMPLETE THIS
-theta_0 = [[r] * 784 for _ in range(10)]  # THETA INITIALIZED HERE
+theta_0 = np.random.randn(num_outputs,num_inputs) / np.sqrt(num_inputs)  # COMPLETE THIS
 
 ALPHA_VAL = 0.003
-
-EPOCH = 100
-M = len(y_train) / 100
-
+EPOCH = 80
 for ep in range(EPOCH):
+    batch_len = 100
     shuffle = np.arange(x_train.shape[0])
     np.random.shuffle(shuffle)
     shuffle_x = x_train[shuffle]
     shuffle_y = y_train[shuffle]
+    for bc in range(0, len(x_test), batch_len):
 
-    gradient_desc = mini_batch(shuffle_x, shuffle_y, theta_0, M)
+        gradient_desc = mini_batch(shuffle_x, shuffle_y, theta_0, bc)
 
- #   gradient_desc = loss_grad(theta_0, shuffle_x, shuffle_y)
-    theta_0 = theta_update(theta_0, gradient_desc, ALPHA_VAL)
+        #   gradient_desc = loss_grad(theta_0, shuffle_x, shuffle_y)
+        theta_0 = theta_update(theta_0, gradient_desc, ALPHA_VAL)
+
+    print(ep)
 
 #######################################################################
+
+# Test Data
+
+total_correct = 0
+
+for n in range( len(x_test)):
+    y = y_test[n]
+    x = x_test[n][:]
+    p = matrix_mult(theta_0, x)
+    prediction = np.argmax(p)
+    if (prediction == y):
+        total_correct += 1
+
+
+print (total_correct/np.float(len(x_test)))
