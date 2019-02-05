ff35c50f1ee565f723e71b5e58584fc43d98e2c0
satwik <satwik.illini@gmail.com>
2019-02-04 22:18:50 -0600

    completed back propogation functions

--- hw2/nn.py
+++ hw2/nn.py
@@ -21,7 +21,7 @@ def softmax(vec):
 
 def linear_step(W, x, b1):
     Wx = np.matmul(W,x)
-    return np.sum(Wx, b1, axis=0)
+    return Wx + b1
 
 # Elementwise ReLU nonlinearity to produce the hidden layer
 def hidden_layer(Z):
@@ -29,20 +29,36 @@ def hidden_layer(Z):
 
 # [000,,,1,,,,00000]
 def e(elem, K):
-    ret = numpy.zeros(K)
+    ret = np.zeros(K)
     ret[elem] = 1
     return ret
 
-def rho(vec, Y):
+def cross_entropy_error(vec, Y):
     return -1*(np.log(vec[Y]))
 
-def partial_U()
+def partial_U(soft, Y):
+    return -1*(Y-soft)
+
+def partial_b2(partial_u):
+    return partial_u
+
+def partial_C(partial_u, H):
+    return np.matmul(partial_u, np.transpose(H))
+
+def partial_b1(delta, Z):
+    return
+
+def partial_W(p_b1, X):
+    return np.matmul(p_b1 ,np.transpose(X))
+
+def param_update(param, ALPHA, grad):
+    return param - (ALPHA*grad)
+    
+    
 
 
 
 
-
-# load MNIST data
 MNIST_data = h5py.File('../MNISTdata.hdf5', 'r')
 
 x_train = np.float32(MNIST_data['x_train'][:])
@@ -55,6 +71,30 @@ MNIST_data.close()
 #######################################################################
 
 # IMPLEMENT Neural Network
+
+num_inputs = 28*28
+num_outputs = 10
+dH = 50
+C = np.random.randn(num_outputs,dH) / np.sqrt(dH)
+W = np.random.randn(dH, num_inputs) / np.sqrt(num_inputs)
+b1 = np.random.randn(dH) / np.sqrt(dH)
+b2 = np.random.randn(num_outputs) / np.sqrt(num_outputs)
+EPOCH = 10
+ALPHA = 1
+
+for ep in range(EPOCH):
+    shuffle = np.arange(x_train.shape[0])
+    np.random.shuffle(shuffle)
+    shuffle_x = x_train[shuffle]
+    shuffle_y = y_train[shuffle]
+    for i in range(len(shuffle_x)):
+        Z = linear_step(W, shuffle_x[i], b1)
+        H = hidden_layer(Z)
+        U = linear_step(C, H, b2)
+        soft_x = softmax(shuffle_x[i])
+        e_y = e(shuffle_y[i], num_outputs)
+        par_U = soft_x - e_y
+        
 
 
 
