50bbc2569f242d00b3e325fef0d2c765a85ac27e
satwik <satwik.illini@gmail.com>
2019-02-14 09:30:43 -0600

    Experimenting with hyperparams

--- Convolution_NN/conv.py
+++ Convolution_NN/conv.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import h5py
-# import time
+import time
 import copy
 from random import randint
 
@@ -18,13 +18,18 @@ def sigma(Z):
 
 # Convolution of matrix X with the filter K
 def convolution(X, K, stride):
-    Z = np.zeros((X.shape[0]-K.shape[0]+1, X.shape[1]-K.shape[1]+1))
-    for m in range(0, Z.shape[0]-1, stride):
-        for n in range(0, Z.shape[1]-1, stride):
-            X_slice = X[m:m+K.shape[0], n:n+K.shape[1]]
-            Z[m,n] = np.vdot(X_slice, K)
-
-    return Z
+    rs = cs = X.itemsize
+    X_slices = np.lib.stride_tricks.as_strided(X, shape = (K.shape[0],K.shape[1], X.shape[0] - K.shape[0] + 1, X.shape[1]-K.shape[1]+1), strides = (X.shape[0]*rs,cs,rs,X.shape[1]*cs))
+    X_s = X_slices.reshape(X_slices.shape[2]*X_slices.shape[3], X_slices.shape[0]*X_slices.shape[1])
+    K_dot = K.reshape(K.shape[0]*K.shape[1], K.shape[2])
+    Conv = np.dot(X_s, K_dot)
+    Conv = Conv.reshape(X_slices.shape[2],X_slices.shape[3], Conv.shape[1])
+    #Z = np.zeros((X.shape[0]-K.shape[0]+1, X.shape[1]-K.shape[1]+1, K.shape[2]))
+    #for m in range(0, Z.shape[0]-1, stride):
+    #    for n in range(0, Z.shape[1]-1, stride):
+    #        for k in range(0, Z.shape[2]):
+    #            Z[m,n,k] = np.vdot(X[m:m+K.shape[0], n:n+K.shape[1]], K[:,:,k])
+    return Conv
 
 def hidden_linear(H, W, bk):
     U = np.zeros((W.shape[0]))
@@ -48,14 +53,7 @@ def softmax(vec):
     return exp_vec
 
 def sigma_prime(Z):
-    for i in range(Z.shape[0]):
-        for j in range(Z.shape[1]):
-            if Z[i,j] >= 0:
-                Z[i,j] = 1
-            else:
-                Z[i,j] = 0
-    
-    return Z
+    return (1/1+np.exp(-1*Z))
 
 
 def partial_K(sigma_prime, delta, X):
@@ -67,13 +65,13 @@ def partial_U(soft, e_Y):
 
 
 def partial_W(partial_b, H):
-    ret = np.zeros((partial_b.shape[0], H.shape[0], H.shape[1]))
+    ret = np.zeros((partial_b.shape[0], H.shape[0], H.shape[1], H.shape[2]))
     for i in range(ret.shape[0]):
         ret[i] = partial_b[i]*H
     return ret
 
 def delta(W, partial_u):
-    delt = np.zeros((W.shape[1], W.shape[2]))
+    delt = np.zeros((W.shape[1], W.shape[2], W.shape[3]))
     for i in range(partial_u.shape[0]):
         delt += partial_u[i]*W[i]
     return delt
@@ -82,12 +80,12 @@ def delta(W, partial_u):
 def param_update(param, ALPHA, grad):
     return param - (ALPHA*grad)
 
-def final_param(param):
-    sum = param[0]
-    for i in range(1, param.shape[0]):
-        sum += param[i]
+# def final_param(param):
+#     sum = param[0]
+#     for i in range(1, param.shape[0]):
+#         sum += param[i]
 
-    return sum
+#     return sum
 
     
 
@@ -104,72 +102,88 @@ y_test = np.int32(np.array(MNIST_data['y_test'][:, 0]))
 MNIST_data.close()
 
 #######################################################################
+def NN(EPOCH, ALPHA, filter_dim, num_filters):
+    # IMPLEMENT Neural Network
 
-# IMPLEMENT Neural Network
+    num_inputs = 28*28
+    input_dim = 28
+    num_outputs = 10
+    K = (2*np.random.random_sample((filter_dim, filter_dim, num_filters)) - 1)/1000
+    b = (2*np.random.random_sample((num_outputs)) - 1) / 1000
+    W = (2*np.random.random_sample((num_outputs, input_dim - filter_dim + 1, input_dim - filter_dim + 1, num_filters)) - 1) / 1000
 
-num_inputs = 28*28
-input_dim = 28
-num_outputs = 10
-filter_dim = 5
-num_filters = 32
-K = np.random.randn(num_filters, filter_dim, filter_dim) / np.sqrt(filter_dim)
-b = np.random.randn(num_outputs) / np.sqrt(num_outputs)
-W = np.random.randn(num_filters, num_outputs, input_dim - filter_dim + 1, input_dim - filter_dim + 1) / np.sqrt(input_dim - filter_dim + 1)
-EPOCH = 1
-ALPHA = 0.003
+    runtime = time.time()
+    for ep in range(EPOCH):
+        start = time.time()
+        print(ep)
+        shuffle = np.arange(x_train.shape[0])
+        np.random.shuffle(shuffle)
+        shuffle_x = x_train[shuffle]
+        shuffle_y = y_train[shuffle]
+        for i in range(len(shuffle_x)):
 
-for ep in range(EPOCH):
-    print(ep)
-    shuffle = np.arange(x_train.shape[0])
-    np.random.shuffle(shuffle)
-    shuffle_x = x_train[shuffle]
-    shuffle_y = y_train[shuffle]
-    for i in range(len(shuffle_x)):
+            X = (shuffle_x[i]).reshape((28,28))
 
-        X = (shuffle_x[i]).reshape((28,28))
-        
-        for k in range(num_filters):
             # FORWARD STEP
-            Z = convolution(X, K[k], 1)
+            Z = convolution(X, K, 1)
             H = sigma(Z)
-            U = hidden_linear(H,W[k],b)
+            U = hidden_linear(H,W,b)
             soft_x = softmax(U)
             e_y = e(shuffle_y[i], num_outputs)
 
             # CALCULATE PARTIAL DERIVATIVES
             par_u = partial_U(soft_x, e_y)
             par_w = partial_W(par_u, H)
-            delt = delta(W[k], par_u)
+            delt = delta(W, par_u)
             par_k = partial_K(sigma_prime(Z), delt, X)
 
             # UPDATE PARAMETERS
 
-            K[k] = param_update(K[k], ALPHA, par_k)
-            W[k] = param_update(W[k], ALPHA, par_w)
+            K = param_update(K, ALPHA, par_k)
+            W = param_update(W, ALPHA, par_w)
             b = param_update(b, ALPHA, par_u)
 
+        # EPOCH ENDED
 
-final_K = final_param(K)
-final_W = final_param(W)
+        total_correct = 0
 
+        for n in range(len(x_test)):
+            y = y_test[n]
+            x = x_test[n][:]
+            x = x.reshape((28,28))
+            Z = convolution(x, K, 1)
+            H = sigma(Z)
+            U = hidden_linear(H, W, b)
+            soft_x = softmax(U)
+            prediction = np.argmax(soft_x)
+            if (prediction == y):
+                total_correct += 1
 
-#######################################################################
-
-# Test Data
-
-total_correct = 0
-
-for n in range(len(x_test)):
-    y = y_test[n]
-    x = x_test[n][:]
-    x = x.reshape((28,28))
-    Z = convolution(x, final_K, 1)
-    H = sigma(Z)
-    U = hidden_linear(H, final_W, b)
-    soft_x = softmax(U)
-    prediction = np.argmax(soft_x)
-    if (prediction == y):
-        total_correct += 1
+        print("Epoch: ", ep)
+        print (total_correct/np.float(len(x_test)))
+        print("Time Taken: ", (time.time() - start))
+        print("Total run time: ", (time.time() - runtime))
 
 
-print (total_correct/np.float(len(x_test)))
+print("20,0.5,5")
+print("128 filters")
+
+NN(20, 0.5, 5, 128)
+
+# print("ALPHA = 0.003, EPOCHS = 1, Filter = 8")
+# NN(1, 0.003, 8)
+# print("Time Taken: ", (time.clock() - start))
+
+# print("ALPHA = 0.003, EPOCHS = 2, Filter = 8")
+# NN(2, 0.003, 8)
+# print("Time Taken: ", (time.clock() - start))
+
+# print("ALPHA = 0.003, EPOCHS = 3, Filter = 8")
+# NN(3, 0.003, 8)
+# print("Time Taken: ", (time.clock() - start))
+
+# print("ALPHA = 0.003, EPOCHS = 2, Filter = 8")
+# NN(4, 0.003, 8)
+# print("Time Taken: ", (time.clock() - start))
+
+
