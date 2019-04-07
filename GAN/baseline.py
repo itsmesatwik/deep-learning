da00b4fe80c379e8f5953e60f77a2a9008e8aed5
satwik <satwik.illini@gmail.com>
2019-04-06 20:07:16 -0500

    added relevant imports

---
+++ GAN/baseline.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torch.nn.functional as F
+
+import torchvision.datasets as datasets
+import torchvision
+import torchvision.transforms as transforms
+
+from torch.autograd import Variable
+
+import math
+import os
+import time
+import numpy as np
